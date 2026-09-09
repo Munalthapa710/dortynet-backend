0ffca3a107e9e75ccff71c4b8fde813d24c7653f
MunalThapa710 <thapamunal710@gmail.com>
2026-09-09 12:50:06 +0545

    changetospandfix

--- EmployeeApi/Service/Intern/InternService.cs
+++ EmployeeApi/Service/Intern/InternService.cs
@@ -1,69 +1,89 @@
-﻿using EmployeeApi.Data;
+﻿using Dapper;
+using EmployeeApi.Data;
+using EmployeeApi.Model.Employee;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 using InternEntity = EmployeeApi.Model.Intern.Intern;
-
 namespace EmployeeApi.Service.Intern
 {
     public class InternService : IInternService
     {
+
+        private readonly string _connString;
         private readonly ApplicationDbContext _context;
 
-        public InternService(ApplicationDbContext context)
+        public InternService(ApplicationDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _connString = configuration.GetConnectionString("DefaultConnection") ?? 
+                throw new InvalidOperationException("Connection string not found.");
+
         }
 
         public async Task<List<InternEntity>> GetAll()
         {
-            return await _context.Interns.ToListAsync();
+            //return await _context.Interns.ToListAsync();
+            using var connection = new SqlConnection(_connString);
+            var employees = await connection.QueryAsync<InternEntity>(
+           "[dbo].[GetAllInterns]",
+            commandType: CommandType.StoredProcedure
+           );
+            return employees.ToList();
         }
 
         public async Task<InternEntity?> GetById(int id)
         {
-            return await _context.Interns.FindAsync(id);
+            //return await _context.Interns.FindAsync(id);
+            using var connection = new SqlConnection(_connString);
+            var employee = await connection.QueryFirstOrDefaultAsync<InternEntity>(
+                "[dbo].[GetInternById]",
+                new { Id = id },
+                commandType: CommandType.StoredProcedure
+            );
+            return employee;
         }
 
         public async Task<InternEntity> Create(InternEntity intern)
         {
-            _context.Interns.Add(intern);
+            using var connection = new SqlConnection(_connString);
+            var createdIntern = await connection.QuerySingleAsync<InternEntity>(
+                "[dbo].[CreateIntern]",
+                new
+                {
+                    intern.Name,
+                    intern.Description
+                },
+                commandType: CommandType.StoredProcedure);
 
-            await _context.SaveChangesAsync();
-
-            return intern;
+            return createdIntern;
         }
 
         public async Task<InternEntity> Update(int id, InternEntity intern)
         {
-            var existingIntern = await _context.Interns.FindAsync(id);
-
-            if (existingIntern == null)
-            {
-                throw new InvalidOperationException("Intern not found");
-            }
-
-            _context.Entry(existingIntern)
-                .CurrentValues
-                .SetValues(intern);
-
-            await _context.SaveChangesAsync();
-
-            return existingIntern;
+            using var connection = new SqlConnection(_connString);
+            var updatedIntern = await connection.QuerySingleOrDefaultAsync<InternEntity>(
+                "[dbo].[UpdateIntern]",
+                new
+                {
+                    Id = id,
+                    intern.Name,
+                    intern.Description
+                },
+                commandType: CommandType.StoredProcedure);
+             return updatedIntern ?? throw new KeyNotFoundException($"Intern {intern.Id} was not found.");
         }
 
         public async Task<bool> Delete(int id)
         {
-            var intern = await _context.Interns.FindAsync(id);
+            using var connection = new SqlConnection(_connString);
+            var deleted = await connection.ExecuteAsync(
+                "[dbo].[DeleteIntern]",
+                new { Id = id },
+                commandType: CommandType.StoredProcedure);
+            return deleted > 0;
+               }
 
-            if (intern == null)
-            {
-                return false;
-            }
-
-            _context.Interns.Remove(intern);
-
-            await _context.SaveChangesAsync();
-
-            return true;
+            
         }
     }
-}
